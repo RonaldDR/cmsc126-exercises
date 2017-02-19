cb27ba1a612b93e91ba685ee58595f338c2df794
Ronald del Rosario <rjdelrosario15@yahoo.com>
2017-02-19 21:25:48 +0800

    latest comm

--- ex1_url_parser/parser.js
+++ ex1_url_parser/parser.js
@@ -21,8 +21,53 @@ function parseUri(uri) {
         query: '',
         fragment: ''
     };
-
     // add your code here
+    var str = uri;
 
+    var pos = str.indexOf(":");
+    if(pos > -1){
+        uriParts.scheme = str.slice(0,pos);
+        str = str.slice(pos+3, str.length+1);
+    }
+    else{
+        uriParts.scheme = str.slice(0,str.length);
+        str = '';
+    }
+
+    pos = str.indexOf("/");
+    if(pos > -1){
+        uriParts.authority = str.slice(0,pos);
+        str = str.slice(pos, str.length);
+    }
+    else{
+        uriParts.authority = str.slice(0, str.length);
+        str = '';
+    }
+
+    pos = str.indexOf("?");
+    if(pos > -1){
+        uriParts.path = str.slice(0,pos);
+        str = str.slice(pos+1, str.length);
+    }
+    else{
+        uriParts.path = str.slice(0,str.length);
+        str = '';
+    }
+
+    pos = str.indexOf("#")
+    if(pos > -1){
+        uriParts.query = str.slice(0,pos);
+        str = str.slice(pos+1, str.length);
+    }
+    else{
+        uriParts.query = str.slice(0,str,length);
+        str = '';
+    }
+
+
+    if(pos > -1){
+        uriParts.fragment = str.slice(0,str.length);
+    }
+    
     return uriParts;
 }
